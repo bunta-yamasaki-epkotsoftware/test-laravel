8f3e2036baefa2b1ef4d59c63d8c4534674bace4
Yamasaki.B <bunta.yamasaki@epkotsoftware.co.jp>
2025-07-14 18:20:09 +0900

    バリデーションエラーメッセージ日本語化の追加

---
+++ my-app/resources/lang/ja/validation.php
@@ -0,0 +1,13 @@
+<?php
+    return [
+        'max' => [
+            'string' => ':attributeは:max文字以下で入力してください。',
+        ],
+        'required' => ':attributeは必須項目です。',
+        'email' => ':attributeは有効なメールアドレス形式で入力してください。',
+        'attributes' => [
+            'name' => '名前',
+            'email' => 'メールアドレス',
+            'message' => 'メッセージ',
+        ],
+    ];
--- my-app/resources/views/contact/index.blade.php
+++ my-app/resources/views/contact/index.blade.php
@@ -8,14 +8,23 @@
         <div class="mb-3">
             <label for="name" class="form-label">お名前</label>
             <input type="text" name="name" id="name" class="form-control" value="{{old('name')}}" required>
+            @if($errors->has('name'))
+                <div class="text-danger">{{ $errors->first('name') }}</div>
+            @endif
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">メールアドレス</label>
             <input type="text" name="email" id="email" class="form-control" value="{{old('email')}}" required>
+            @error('email')
+                <div class="text-danger">{{ $errors->first('email') }}</div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="message" class="form-label">メッセージ</label>
             <textarea name="message" id="message" class="form-control" rows="5" required>{{old('message')}}</textarea>
+            @error('message')
+                <div class="text-danger">{{ $errors->first('message') }}</div>
+            @enderror
         </div>
         <button type="submit" class="btn btn-primary">確認画面へ</button>
     </form>
